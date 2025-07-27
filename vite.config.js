54f984191fe2eb606b1e232e6ea9ede2b1a0ae25
zeetonia1 <zaher.lp1@gmail.com>
2025-07-27 11:13:13 +0300

    update deploy setting

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Digital-Agency-landing"
+  base: process.env.NODE_ENV === 'production' ? '/Digital-Agency-landing/' : '/',
 })
